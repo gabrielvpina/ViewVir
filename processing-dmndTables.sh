e8c8cf9921bd26f3f90c6855833840bab1d823fe
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-04-07 02:07:22 -0300

    Add files via upload

---
+++ processing-dmndTables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Criar um novo diretório
+dir_name="diamond-processed"
+mkdir -p "$dir_name"
+
+# Obter a lista de arquivos na pasta diamond-output/
+lista_arquivos=$(ls diamond-output/)
+
+# Iterar sobre os arquivos na lista
+for arquivo in $lista_arquivos; do
+    # Caminho de entrada e saída para o Fastp
+    entrada="diamond-output/$arquivo"
+    saida="$dir_name/$arquivo"
+
+
+sed -i 's/\[/\t/g; s/\]//g' $entrada
+
+
+echo "QuerySeq	SubjectSeq	QseqLength	SseqLength	Pident	Evalue	BitScore	SubjTitle	Specie	FullQueryLength" | cat - $entrada > $saida 
+# && mv $saida $entrada
+
+done 
