71c02b575c05181d1d05ec9250a64695dd596c26
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-04-08 18:15:34 -0300

    Update ProcessNPlot.R

--- ProcessNPlot.R
+++ ProcessNPlot.R
@@ -1,10 +1,17 @@
+if (!require("here", quietly = TRUE))
+install.packages("here")
+library(here)
+
 # Insert the directory of diamond-processed file
-WorkingDir = "./"
+WorkingDir = here()
 setwd(WorkingDir)
 
 # Required packages
 if (!require("dplyr", quietly = TRUE))
 install.packages("dplyr")
+
+if (!require("pandoc", quietly = TRUE))
+install.packages("pandoc")
 
 if (!require("plotly", quietly = TRUE))
 install.packages("plotly")
@@ -16,13 +23,15 @@ if (!require("htmltools", quietly = TRUE))
 install.packages("htmltools")
 
 if (!require("ggplot2", quietly = TRUE))
-  install.packages("ggplot2")
+install.packages("ggplot2")
 
 library(dplyr)
 library(ggplot2)
 library(plotly)
 library(gapminder)
 library(htmlwidgets)
+library(pandoc)
+
 
 
 # import all tables from ICTV
@@ -35,10 +44,12 @@ ictvVMR_virNames <- read.csv("ICTV_data/ICTV_VMR-virNames.csv")
 # merge all tables with rbind
 allVirus <- rbind(ictvMSL, ictvVMR_species, ictvVMR_virNames)
 
-arquivos <- list.files(path = "/home/gabriel/bioinfo/bioinfo_scripts/scripts_viral/diamond-processed", pattern = "\\.tsv$", full.names = TRUE)
+arquivos <- list.files(path = here("diamond-processed"), pattern = "\\.tsv$", full.names = TRUE)
 
 # Crie a pasta para os resultados
-dir.create("diamond-results")
+if (!dir.exists("ViewVir-results")) {
+  dir.create("ViewVir-results")
+}
 
 # Loop para cada tabela .tsv
 for (arquivo_path in arquivos) {
@@ -54,16 +65,38 @@ for (arquivo_path in arquivos) {
   outrasCols <- setdiff(names(arquivo), "Genome.composition")
   arquivo <- arquivo[, c("QuerySeq", "SubjectSeq", "QseqLength", "SseqLength", "Pident", "Evalue", "BitScore", "SubjTitle",
                          "Species", "Genome.composition", "FullQueryLength")]
+
   
+############################## OLD VERSION OUTPUT ###########################################  
+  # Obtenha o nome do arquivo de entrada
+  #nome_arquivo_input <- basename(arquivo_path)
+  
+  
+  # Defina o caminho completo do arquivo de saída, incluindo o nome da pasta
+  #caminho_arquivo_output <- file.path("ViewVir-results/", sub(".tsv", "_output.tsv", nome_arquivo_input))
+  #plot_output <- file.path("ViewVir-results", sub(".tsv","_bubblePlt.html", nome_arquivo_input))
+  
+  # Escreva os dados em um arquivo de saída
+  #write.table(arquivo, file = caminho_arquivo_output, sep = "\t")
+################################################################################  
+  
+  
+#################### função subpastas (teste) ###################################  
   # Obtenha o nome do arquivo de entrada
   nome_arquivo_input <- basename(arquivo_path)
   
-  # Defina o caminho completo do arquivo de saída, incluindo o nome da pasta
-  caminho_arquivo_output <- file.path("diamond-results", sub(".tsv", "_output.tsv", nome_arquivo_input))
-  plot_output <- file.path("diamond-results", sub(".tsv","_bubblePlt.html", nome_arquivo_input))
+  # Crie o diretório com o nome do arquivo dentro de "diamond-results"
+  pasta_nome_arquivo <- file.path("ViewVir-results", gsub(".fasta.tsv", "", nome_arquivo_input))
   
-  # Escreva os dados em um arquivo de saída
+  dir.create(pasta_nome_arquivo, showWarnings = FALSE)
+  
+  # Defina o caminho completo do arquivo de saída dentro do diretório criado
+  caminho_arquivo_output <- file.path(pasta_nome_arquivo, paste0(sub(".tsv", "_output.tsv", nome_arquivo_input)))
+  plot_output <- file.path(pasta_nome_arquivo, paste0(sub(".tsv", "_bubblePlt.html", nome_arquivo_input)))
+  
+  # Escreva os dados em um arquivo de saída dentro do diretório criado
   write.table(arquivo, file = caminho_arquivo_output, sep = "\t")
+##################################################################################
   
 # Grafico Interativo
   
@@ -80,28 +113,4 @@ for (arquivo_path in arquivos) {
   htmlwidgets::saveWidget(plot, file = plot_output, selfcontained = TRUE)
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
 
