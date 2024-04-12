109d08fc00adbb36582c31697bda81b83e6a2198
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-04-12 17:39:37 -0300

    Add files via upload

---
+++ scripts/cap3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Criar um novo diretório
+dir_name="cap3-output"
+mkdir -p "$dir_name"
+
+# Obter a lista de arquivos na pasta STAR_unmapped/
+lista_arquivos=$(ls "nonHost_sequences/")
+
+# Iterar sobre os arquivos na lista
+for arquivo in $lista_arquivos; do
+    # Caminho de entrada e saída para o Fastp
+    entrada="nonHost_sequences/$arquivo"
+    
+    saida="$dir_name/$arquivo"
+
+cap3 $entrada >> $saida
+    
+done
+
+
