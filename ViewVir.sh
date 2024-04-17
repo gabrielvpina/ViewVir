a306b90199f3e602b2ee232545530f41c6939c00
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-04-17 00:57:12 -0300

    Update ViewVir.sh

--- ViewVir.sh
+++ ViewVir.sh
@@ -2,10 +2,17 @@
 
 chmod +x scripts/*.sh
 
+
+scripts/./cap3.sh
+
+scripts/./proc-cap3-out.sh
+
 scripts/./diamondScript.sh && \
 
 scripts/./processing-dmndTables.sh && \
 
 Rscript scripts/results.R && \
+
+scripts/./criando_fasta.sh && \
 
 rm -r diamond-processed
