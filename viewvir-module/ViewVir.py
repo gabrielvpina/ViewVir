e87ac03d2f2ef86e0bfe9be6314aac77abb9f4d0
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-06-20 18:36:28 -0300

    Update ViewVir.py

--- viewvir-module/ViewVir.py
+++ viewvir-module/ViewVir.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 from modules.tblfmt import process_diamondTbl
+from modules.findorf import findorf
+
+vvfolder = "ViewVir-results"
 
 # Importando dados
 ncbiSpecie = pd.read_csv("data/NCBI_virSpecies.csv", names=["Species", "Genome.composition"])
@@ -12,4 +15,8 @@ allVirus = pd.concat([ncbiNames, ncbiSpecie])
 # Processando dados
 print("Enter the diamond table")
 dmndtable = input()
-process_diamondTbl(dmndtable)
+process_diamondTbl(dmndtable,vvfolder)
+
+# Criar Orfs
+findorf(vvfolder)
+
