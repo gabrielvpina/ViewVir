efd12720676a7520bdb08eb23567d9e0281a195a
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-06-20 17:15:36 -0300

    Update ViewVir.py

--- viewvir-module/ViewVir.py
+++ viewvir-module/ViewVir.py
@@ -9,15 +9,7 @@ ncbiNames = pd.read_csv("data/NCBI_virName.csv", names=["Species", "Genome.compo
 # Merge de todas as tabelas
 allVirus = pd.concat([ncbiNames, ncbiSpecie])
 
-# Crie a pasta para os resultados
-#viewvirFolder = "ViewVir-results"
-#if not os.path.exists(viewvirFolder):
-#    os.makedirs(viewvirFolder)
-
 # Processando dados
 print("Enter the diamond table")
 dmndtable = input()
 process_diamondTbl(dmndtable)
-
-
-
