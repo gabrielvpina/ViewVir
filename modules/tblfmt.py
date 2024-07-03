6306310b093ea2e963e6eb920b07f22495c4b751
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-07-03 10:04:07 -0300

    Update tblfmt.py

--- modules/tblfmt.py
+++ modules/tblfmt.py
@@ -8,43 +8,43 @@ allVirus = pd.concat([ncbiNames, ncbiSpecie])
 
 def viralFilter(vvfolder):
 
-    # Pasta para os resultados
+    # result directory
     viewvirFolder = vvfolder
     if not os.path.exists(viewvirFolder):
         os.makedirs(viewvirFolder)
 
     for file_path in os.listdir(vvfolder): 
         if file_path.endswith("_proc.tsv"):
-            # Processando a Tabela
-            # Leia o arquivo
+          
+            
             file = pd.read_csv(os.path.join(vvfolder,file_path), sep="\t")
 
-            # operação de junção
+            # join
             file = pd.merge(file, allVirus, on="Species", how="left")
 
-            # ordem das colunas
+            # change order
             outrasCols = list(set(file.columns) - {"Genome.composition"})
             file = file[["QuerySeq", "SubjectSeq", "QseqLength", "SseqLength", "Pident", "Evalue", "QCover",
                       "SubjTitle", "Species", "Genome.composition", "FullQueryLength"]]
 
-            # Filtro de sequencias
+            # filter length
             file = file[file["QseqLength"] >= 500]
 
-            # Remover duplicatas
+            # remove duplicates
             file = file.drop_duplicates()
-            # Obter o nome base do arquivo de entrada
+            
             inputBasename = os.path.basename(file_path)
-            # Caminho do arquivo de saída
+            # out path
             output_file = os.path.join(viewvirFolder, inputBasename.replace("_proc.tsv", "_processed.tsv"))
     
-            # Escreva os dados em um arquivo de saída dentro do diretório criado
+            # write table
             file.to_csv(output_file, sep="\t", index=False)
 
 
-            # Filtro para vírus de RNA
+            # non-dna filter
             tableRNA = file[file["Genome.composition"].isin(["ssRNA(-)", "ssRNA(+/-)", "ssRNA(+)", "ssRNA-RT", "RNA", "unknown", "NA"])]
 
-            # Dados em um arquivo de saída separado para vírus não DNA
+            # non-dna table
             nonDNA_table = os.path.join(viewvirFolder, inputBasename.replace("_proc.tsv", "_nonDNA.tsv"))
             tableRNA.to_csv(nonDNA_table, sep="\t", index=False)
 
@@ -55,11 +55,11 @@ def viralFilter(vvfolder):
             files = os.listdir(vvfolder)
 
             output_pre = os.path.join(vvfolder, inputBasename.replace("_nonDNA.tsv", "_pre.tsv"))
-            #tableRNA = pd.read_csv(vvfolder + nonDNA)
+            
             arquivo_PRE = tableRNA[["QuerySeq", "FullQueryLength"]]
             arquivo_PRE.to_csv(output_pre, sep="\t", index=False)
 
-            # Processar _pre.tsv para gerar RNA-virus.fasta
+            # generate fasta 
             samp = os.path.basename(output_pre).replace("_pre.tsv", "")
             with open(output_pre, "r") as infile, open(f"{vvfolder}/{samp}_nonDNA.fasta", "w") as outfile:
                 lines = infile.readlines()
@@ -77,21 +77,21 @@ def renameFasta(outputFolder):
     for fasta in os.listdir(outputFolder):
         if fasta.endswith(".fasta"):
             input_fasta = os.path.join(outputFolder, fasta)
-    # Lista para armazenar os registros modificados
+    
     registros_modificados = []
 
-    # Iterar sobre os registros no arquivo FASTA de entrada
+    
     for i, record in enumerate(SeqIO.parse(input_fasta, "fasta"), start=1):
-        # Modificar o cabeçalho
+        
         novo_id = f"contig_{i:02d}"
         record.id = novo_id
         record.description = novo_id  # Atualizar a descrição também
 
-        # Adicionar o registro modificado à lista
+        
         registros_modificados.append(record)
 
     fastafile = os.path.join
-    # Escrever os registros modificados no novo arquivo FASTA
+    
     with open(input_fasta, "w") as output_handle:
         SeqIO.write(registros_modificados, output_handle, "fasta")
 
