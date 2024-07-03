bd868e0dee65cc7388511af48d05cd7e86cf47d7
Gabriel Rodrigues <69880032+gabrielvpina@users.noreply.github.com>
2024-07-03 09:54:41 -0300

    Update contigProcess.py

--- modules/contigProcess.py
+++ modules/contigProcess.py
@@ -3,17 +3,17 @@ import subprocess
 import pandas as pd
 
 def cap3(inputContig,outputFolder):
-    # Pasta para os resultados
+    # directory w/ results
     if not os.path.exists(outputFolder):
         os.makedirs(outputFolder)
 
-    # Copiar o arquivo de entrada para a pasta de saída
+    # copy original fasta in result directory
     copy_command = ["cp", inputContig, outputFolder]
     subprocess.run(copy_command, check=True)
 
     for cpContig in os.listdir(outputFolder):
         if cpContig.endswith(".fasta"):
-            # Executar CAP3
+            # run CAP3
             cap3_command = ["cap3", os.path.join(outputFolder,cpContig)]
             subprocess.run(cap3_command, check=True)
 
@@ -28,19 +28,19 @@ def cap3(inputContig,outputFolder):
                     cat_command = f"cat {os.path.join(outputFolder,contigs)} {os.path.join(outputFolder,singlets)} >> {os.path.join(outputFolder, f'{sample}_merged.fasta')}"
                     print(cat_command)
                     subprocess.run(cat_command, shell=True, check=True)
-                    # O resultado é o arquivo sample_merged.fasta + o arquivo .fasta original
+                    # merge the outputs of cap3
     
     
     log_files = [os.path.join(outputFolder, file) for file in os.listdir(outputFolder)
                  if file.endswith((".links",".qual",".info",".contigs",".singlets",".ace"))]
     for log_file in log_files:
         os.remove(log_file)
-    # Remover arquivo original da pasta
+    # remove original fasta file
     os.remove(os.path.join(outputFolder,inputContig))
 
 
 def diamondTable(viralDB, outputFolder,CPU):
-    # Verificar se o diretório existe
+    # debugs
     if not os.path.exists(outputFolder):
         raise FileNotFoundError(f"O diretório {outputFolder} não existe.")
 
@@ -50,7 +50,7 @@ def diamondTable(viralDB, outputFolder,CPU):
 
             command_dmnd = f"diamond blastx -d {viralDB} -q {os.path.join(outputFolder, inputContig)} --threads {CPU} --outfmt 6 qseqid sseqid qlen slen pident evalue qcovhsp stitle full_qseq --max-target-seqs 1 --out {os.path.join(outputFolder, sample + '_diamond.tsv')}"
             
-            # Executar o comando diamond
+            # run diamond
             try:
                 subprocess.run(command_dmnd, shell=True, check=True)
                 print(f"Processado: {sample}")
@@ -65,22 +65,22 @@ def processDmndOut(outputFolder):
             entrada = os.path.join(outputFolder, table)
             saida = os.path.join(outputFolder, sample)
             
-            # Comando sed para substituir colchetes por tabulações e remover colchetes de fechamento
+            # replace w/ sed - important
             sed_com = f"sed 's/\\[/\\t/g; s/\\]//g' {entrada}"
             
-            # Cabeçalho para adicionar ao arquivo processado
+            # headers of table
             newcols = "QuerySeq\tSubjectSeq\tQseqLength\tSseqLength\tPident\tEvalue\tQCover\tSubjTitle\tSpecies\tFullQueryLength\n"
             
             try:
-                # Executar comando sed e capturar a saída
+                # run sed
                 result = subprocess.run(sed_com, shell=True, check=True, capture_output=True, text=True)
                 
-                # Escrever o cabeçalho e a saída do sed no arquivo de saída
+                # write headers in final table
                 with open(saida, 'w') as out_file:
                     out_file.write(newcols)
                     out_file.write(result.stdout)
                 
-                # Remover a tabela original
+                # remove original table
                 os.remove(entrada)
             except subprocess.CalledProcessError as e:
                 print(f"Erro ao processar o arquivo {entrada}: {e}")
